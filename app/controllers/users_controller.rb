f8f4865aec262b6d6fd0509f0950149d51d9fc62
yoshimoto0719 <115342937+yoshimoto0719@users.noreply.github.com>
2022-11-05 10:33:26 +0000

    [Pspec 実施]

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,51 @@
+class UsersController < ApplicationController
+  
+  before_action :authenticate_user!
+
+  def index
+    @user = current_user
+    @users = User.all
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = Book.where(user_id: @user.id)
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    if @user == current_user
+      render :edit
+    else
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice]="You have updated user successfully."
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+  
+  def correct_user
+    @book = Book.find(params[:id])
+    if currect_user != @book.user
+       redirect_to user_path(currect_user)
+    end
+  end
+
+
+end
